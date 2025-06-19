be870debb5e1cdd08c2b28fef93c2b96b76d92fd
tracer-ysyx <tracer@ysyx.org>
2025-06-20 00:16:14 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:16:14 up  8:31,  2 users,  load average: 0.48, 0.44, 0.62

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -77,7 +77,7 @@ static int cmd_x(char *args){
     printf("startAddr: 0x%08X     ", expResult);
     uint8_t *realAddr= guest_to_host(expResult);
 
-    for(uint i=0;i< atoi(stringLen);++i){
+    for(uint i=atoi(stringLen)-1;i>=0;--i){
         printf("%02X ",*(realAddr+i));
     }
     printf("\n");
