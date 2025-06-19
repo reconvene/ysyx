a6e177d29d19aa269fafdbea79b441d4e5aa7c0d
tracer-ysyx <tracer@ysyx.org>
2025-06-20 04:54:13 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:54:13 up 13:09,  2 users,  load average: 0.79, 0.42, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -57,7 +57,7 @@ static struct rule {
   {"\\(",TK_LEFT_SP},
   {"\\)",TK_RIGHT_SP},
   {"\\^",TK_POW},
-  {"[0-9]+",TK_NUM}
+  {"(-[0-9]+|[0-9]+)",TK_NUM}
 
 };
 
@@ -104,8 +104,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+//        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+//            i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
