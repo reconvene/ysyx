62df4a16ced773705f7e1df7264c455513e5a1b1
tracer-ysyx <tracer@ysyx.org>
2025-06-20 04:10:42 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:10:42 up 12:25,  2 users,  load average: 0.23, 0.42, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -191,7 +191,6 @@ int eval(uint8_t start, uint8_t end){
   } else{
     uint8_t opPosition=0;
     uint8_t SPNum=0;
-    printf("start:%d end:%d\n",start,end);
     // 遍历找到运算符位置
     for(uint8_t i=start;i<=end;++i){
       // 如果找到运算符且其没有被表达式包裹则设为主运算符
@@ -204,6 +203,7 @@ int eval(uint8_t start, uint8_t end){
     // 对俩边表达式进行求值
     uint8_t leftValue= eval(start,opPosition-1);
     uint8_t rightValue= eval(opPosition+1,end);
+    printf("start:%d end:%d\n",start,end);
     printf("leftValue:%d\n",leftValue);
     printf("rightValue:%d\n",rightValue);
 
