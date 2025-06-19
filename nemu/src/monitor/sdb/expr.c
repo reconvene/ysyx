d064a52d0fbc5d47f08003d3dd062ef17afd072b
tracer-ysyx <tracer@ysyx.org>
2025-06-20 04:58:36 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:58:36 up 13:13,  2 users,  load average: 0.65, 0.43, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -230,8 +230,8 @@ int eval(uint8_t start, uint8_t end){
 
     // 对俩边表达式进行求值
     printf("%d%s\n",opPosition,tokens[opPosition].str);
-    uint8_t leftValue= eval(start,opPosition-1);
-    uint8_t rightValue= eval(opPosition+1,end);
+    int leftValue= eval(start,opPosition-1);
+    int rightValue= eval(opPosition+1,end);
 //    printf("start:%d end:%d\n",start,end);
     printf("leftValue:%d\n",leftValue);
     printf("rightValue:%d\n",rightValue);
