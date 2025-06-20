6bb993185a7fba458d91cd8d5bf3e69ece7431c4
tracer-ysyx <tracer@ysyx.org>
2025-06-20 15:59:32 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:59:32 up  3:32,  0 users,  load average: 1.33, 1.31, 1.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -224,7 +224,7 @@ long int eval(uint8_t start, uint8_t end){
 //      printf("%d  %s\n",i,tokens[i].str);
       // 如果找到运算符且其没有被表达式包裹则设为主运算符
       // 判断运算符的优先级，在预算符相同优先级时选择最左边的
-      if(tokens[i].type>=TK_PLUS && SPNum==0 && judgeLevel(tokens[i].type)<tmpPriority) {
+      if(tokens[i].type>260 && SPNum==0 && judgeLevel(tokens[i].type)<tmpPriority) {
           opPosition=i;
 
           tmpPriority=judgeLevel(tokens[i].type);
