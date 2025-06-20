c61435e75fe5fbc359565e0afe4863a850d6e1c0
tracer-ysyx <tracer@ysyx.org>
2025-06-20 18:24:02 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:24:02 up  5:56,  0 users,  load average: 0.23, 0.34, 0.41

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -243,10 +243,11 @@ long int eval(uint8_t start, uint8_t end){
       // 判断运算符的优先级，在预算符相同优先级时选择最右边的
       if(tokens[i].type>260 && tokens[i].type!=TK_NUM && SPNum==0 && judgeLevel(tokens[i].type)<=tmpPriority) {
           opPosition=i;
-          printf("priority:%d\n",judgeLevel(tokens[i].type));
-          printf("type:%d\n",tokens[i].type);
+
           tmpPriority=judgeLevel(tokens[i].type);
       }
+      printf("priority:%d\n",judgeLevel(tokens[i].type));
+      printf("type:%d\n",tokens[i].type);
     }
 
     // 对右边表达式进行求值;
