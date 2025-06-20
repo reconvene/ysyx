329ff955afdc48a87e2d5d10be81ef7fb8a74a70
tracer-ysyx <tracer@ysyx.org>
2025-06-20 21:22:56 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:22:56 up  8:55,  1 user,  load average: 0.66, 0.54, 0.60

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -63,7 +63,7 @@ static int cmd_si(char *args){
 static int cmd_info(char *args){
   if(strcmp(args,"r")==0){
     isa_reg_display();
-  }else if(strcmp(args,"w")){
+  }else if(strcmp(args,"w")==0){
     list_wp();
   }else{
     puts("invalid input");
