b4545b984784cb7c8a265c54872c2ad46699c8f4
tracer-ysyx <tracer@ysyx.org>
2025-06-20 22:22:08 +0800

    > compile NEMU ysyx_100015735 左余杭 Linux ubuntu2204 5.15.0-141-generic #151-Ubuntu SMP Sun May 18 21:35:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:22:08 up  9:54,  1 user,  load average: 0.43, 0.37, 0.43

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -137,15 +137,14 @@ WP *monitor_wp(){
   }
   WP *changingWP=head;
   _Bool *calcuState=false;
-  printf("%d\n",114514);
 
   // 获取每个监视点的值，并与过去值比较
   while(changingWP!=NULL){
     // 如果未初始化，则初始化
     if(!changingWP->initialState){
       printf("%d\n",114514);
+      printf("%d\n", expr(changingWP->exp,calcuState));
       changingWP->resultValue= expr(changingWP->exp,calcuState);
-      printf("%d\n",114514);
       printf("%d\n",changingWP->resultValue);
 //      Assert(!calcuState,"the watchpoint named %s has some calculation errors", changingWP->wpName);
       changingWP->initialState=true;
