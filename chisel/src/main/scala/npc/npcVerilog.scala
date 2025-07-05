c6352f5d4cc64b0523d2d258be787ee52313f22e
25060181-Yuhang Zuo <sporting_scale511@aleeas.com>
2025-07-05 09:23:10 +0800

    Initialize npc subproject for riscv32
    Set up basic instruction decoding framework for riscv32
    
    Verify decoding and execution of addi instruction
    
    Update .gitignore

---
+++ chisel/src/main/scala/npc/npcVerilog.scala
@@ -0,0 +1,15 @@
+// See README.md for license details.
+
+package npc
+
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+
+import _root_.circt.stage.ChiselStage
+
+object npcVerilog extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new npc,
+    args,
+    firtoolOpts = Array("-disable-all-randomization")
+  )
+}
